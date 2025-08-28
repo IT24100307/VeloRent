e6c815cd778ee845f3dbd3ca47380af1557e9e38
IT24100307 <it24100307@my.sliit.lk>
2025-08-28 20:27:00 +0530

    Uploading project structure

---
+++ Car-Rental-System/src/main/java/Group2/Car/Rental/System/CarRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package Group2.Car.Rental.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalSystemApplication.class, args);
+	}
+
+}
---
+++ Car-Rental-System/src/test/java/Group2/Car/Rental/System/CarRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Group2.Car.Rental.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
